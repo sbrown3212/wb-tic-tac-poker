a6814ec595acdc343a0ecd023dbdb2320436afcf
Stephen Brown <stephen.jr36@gmail.com>
2024-08-08 16:09:59 -0600

    completed part2. Completed lab.

--- Part2/index.js
+++ Part2/index.js
@@ -4,9 +4,8 @@ const colorInput = document.querySelector('#style');
 function setCard() {
     // QUESTION: When do I use .value and when do I use .innerText?
     const card = document.querySelector(`#${idInput.value}`);
-    console.log(card);
+    
+    card.style.color = colorInput.value;
 }
 
 document.querySelector('#editCardButton').addEventListener('click', setCard)
-
-console.log(idInput)
